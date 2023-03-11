a008f76172f6ec899075ac898c10ca39872cbfc8
kartikgupta <kartik.gupta2505@gmail.com>
2023-03-11 14:21:30 +0530

    about section writeup

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -2,9 +2,8 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div style={{minHeight: "90vh"}}>
-        This is about component
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti tenetur possimus fuga error expedita sequi sapiente? Temporibus quod maxime itaque, repellat reprehenderit beatae esse possimus incidunt quisquam sunt sequi alias?</p>
+    <div style={{minHeight: "90vh",margin:'5px 5px 5px 5px',padding:'5px 5px 5px 5px'}}>
+        <p><h5>Welcome to the React Todo List app! This app was created to help you manage your daily tasks and stay organized. With its simple and intuitive interface, you can easily add and delete tasks as needed.</h5></p>
     </div>
   )
 }
